70ecaf19a95901cd5e0dc4c88905345410b80be9
Svetlana <sveta51151378@yandex.ru>
2025-04-13 15:28:21 +0300

    Add new class

---
+++ src/main/java/org/example/Cat.java
@@ -0,0 +1,39 @@
+package org.example;
+
+public class Cat {
+    private String name;
+
+    private String color;
+
+    private int age;
+
+    public Cat(String color, int age, String name) {
+        this.color = color;
+        this.age = age;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,15 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
-        System.out.printf("Second commit!");
+        System.out.println("Second commit!");
+
+        Cat firstCat = new Cat("Борис", 2, "бежевый");
+        Cat secondCat = new Cat("Мурзик", 3, "черно-белый");
+        System.out.println("Данные первого кота: " + firstCat.getName()
+                + ", " + firstCat.getColor() + "," + firstCat.getAge());
+
+        System.out.println("Данные второго кота: " + secondCat.getName()
+                + ", " + secondCat.getColor() + "," + secondCat.getAge());
     }
+
 }
